30315f46601a326ce756bf26c8e72ed74fce0c40
himanshumathur <mathurhimanshu2@gmail.com>
2024-02-22 12:08:18 +0530

    initial commit

---
+++ src/main/java/com/tekion/cricketgame/CricketgameApplication.java
@@ -0,0 +1,13 @@
+package com.tekion.cricketgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketgameApplication.class, args);
+	}
+
+}
