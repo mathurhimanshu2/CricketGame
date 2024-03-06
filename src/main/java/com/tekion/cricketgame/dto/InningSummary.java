949bd75210e1fbc15f20bba51b47252361d51f8c
himanshumathur <mathurhimanshu2@gmail.com>
2024-03-07 00:39:40 +0530

    Resolved Comments

--- src/main/java/com/tekion/cricketgame/dto/InningSummary.java
+++ src/main/java/com/tekion/cricketgame/dto/InningSummary.java
@@ -1,5 +1,12 @@
 package com.tekion.cricketgame.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InningSummary {
 
     private BattingDetails battingDetails;
@@ -8,47 +15,4 @@ public class InningSummary {
     private int teamRuns;
     private int teamWickets;
 
-    public InningSummary(){
-
-    }
-
-
-    public InningSummary(BattingDetails battingDetails, BowlingDetails bowlingDetails, int teamRuns, int teamWickets) {
-        this.battingDetails = battingDetails;
-        this.bowlingDetails = bowlingDetails;
-        this.teamRuns = teamRuns;
-        this.teamWickets = teamWickets;
-    }
-
-    public BattingDetails getBattingDetails() {
-        return battingDetails;
-    }
-
-    public void setBattingDetails(BattingDetails battingDetails) {
-        this.battingDetails = battingDetails;
-    }
-
-    public BowlingDetails getBowlingDetails() {
-        return bowlingDetails;
-    }
-
-    public void setBowlingDetails(BowlingDetails bowlingDetails) {
-        this.bowlingDetails = bowlingDetails;
-    }
-
-    public int getTeamRuns() {
-        return teamRuns;
-    }
-
-    public void setTeamRuns(int teamRuns) {
-        this.teamRuns = teamRuns;
-    }
-
-    public int getTeamWickets() {
-        return teamWickets;
-    }
-
-    public void setTeamWickets(int teamWickets) {
-        this.teamWickets = teamWickets;
-    }
 }
